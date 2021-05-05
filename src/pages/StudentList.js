04ddcf1f6634f92a18051c5421ffb219bffb1f06
jameelappsintegra <mail4jameelmca@gmail.com>
2021-05-05 21:43:11 +0400

    dummy data addede

--- src/pages/StudentList.js
+++ src/pages/StudentList.js
@@ -24,37 +24,76 @@ const StudentList = () => {
 
 const data = [
     {
+        "id": "1",
         "img": "",
         "name": "Hind Khamis",
         "class": "VI",
         "roll": "02",
+        "admissionDate": "20-Apr-2016",
+        "bloodGroup": "o+",
+        "gender": "Male",
+        "status": "Active",
+        "Adress": "Dubai",
+        "dob": "19-Apr-2018",
+        "gpa": "80",
         "year": {
             "from": "2020",
             "to": "2021"
-        }
-
+        },
+        "attandance": [
+            {
+                "start": "10-Apr-2005",
+                "end": ""
+            }
+        ]
     },
     {
+        "id": "2",
         "img": "",
         "name": "Mohamed Obaid",
         "class": "V",
         "roll": "03",
+        "admissionDate": "20-Apr-2016",
+        "bloodGroup": "o+",
+        "gender": "Male",
+        "status": "Active",
+        "Adress": "Dubai",
+        "dob": "19-Apr-2018",
+        "gpa": "80",
         "year": {
             "from": "2020",
             "to": "2003"
-        }
-
+        },
+        "attandance": [
+            {
+                "start": "10-Apr-2005",
+                "end": ""
+            }
+        ]
     },
     {
+        "id": "3",
         "img": "",
         "name": "Meera Salem",
         "class": "III",
         "roll": "01",
+        "admissionDate": "20-Apr-2016",
+        "bloodGroup": "o+",
+        "gender": "Male",
+        "status": "Active",
+        "Adress": "Dubai",
+        "dob": "19-Apr-2018",
+        "gpa": "80",
         "year": {
             "from": "2019",
             "to": "2020"
-        }
-
+        },
+        "attandance": [
+            {
+                "start": "10-Apr-2005",
+                "end": ""
+            }
+        ],
     },
 ]
 
