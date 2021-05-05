79379aaa74f0c71a6eee610b9599f88e19b4306c
jameelappsintegra <mail4jameelmca@gmail.com>
2021-05-05 20:52:06 +0400

    changes

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -11,14 +11,29 @@ import TimeTable from "../assets/icons/ic_calendra.svg";
 
 function Main() {
     return (
-        <div className="student-cards">
-            <Card copy="Home Work" icon={quiz} link="student-details" />
-            <Card copy="Syllabus" icon={Syllabus} link="student-list" />
-            <Card copy="Attendance" icon={Attendance} />
-            <Card copy="School Hoildays" icon={Hoildays} />
-            <Card copy="Results" icon={Results} />
-            <Card copy="Time Table" icon={TimeTable} />
-        </div>
+        <>
+
+            <div className="card contact-card">
+                <div className="card-body">
+                    <div className="contact-row">
+                        <i className="las la-phone"></i>
+                    +91 987892345656
+                </div>
+                    <div className="contact-row">
+                        <i className="las la-envelope"></i>
+                    ansushsh@email.com
+                </div>
+                </div>
+            </div>
+            <div className="student-cards">
+                <Card copy="Home Work" icon={quiz} link="student-details" />
+                <Card copy="Syllabus" icon={Syllabus} link="student-list" />
+                <Card copy="Attendance" icon={Attendance} />
+                <Card copy="School Hoildays" icon={Hoildays} />
+                <Card copy="Results" icon={Results} />
+                <Card copy="Time Table" icon={TimeTable} />
+            </div>
+        </>
     )
 }
 export default Main;
--- src/pages/StudentProfile.js
+++ src/pages/StudentProfile.js
@@ -1,28 +1,17 @@
 import React from 'react';
 
-const StudentProfile = (props)=>{
+const StudentProfile = (props) => {
     return (
         <>
-        <div className="student-profile">
-            <div>
-                <h3>Ansh Sharma</h3>
-                <small>Class XI-B | Roll no: 04</small>
-                <h6 className="sub-title">2020-2021</h6>
-            </div>
-            <img src={props.studentPhoto} alt="Ansh Sharma" width="48"  />
-        </div>
-        <div className="card contact-card">
-            <div className="card-body">
-                <div className="contact-row">
-                    <i className="las la-phone"></i>
-                    +91 987892345656
+            <div className="student-profile">
+                <div>
+                    <h3>Ansh Sharma</h3>
+                    <small>Class XI-B | Roll no: 04</small>
+                    <h6 className="sub-title">2020-2021</h6>
                 </div>
-                <div className="contact-row">
-                    <i className="las la-envelope"></i>
-                    ansushsh@email.com
-                </div>
+                <img src={props.studentPhoto} alt="Ansh Sharma" width="48" />
             </div>
-        </div>
+
         </>
     )
 }
