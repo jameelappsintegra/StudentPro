5cc2ec204c76c82258324fb13241448e02d05fbc
jameelappsintegra <mail4jameelmca@gmail.com>
2021-05-06 23:23:35 +0400

    card link changed

--- src/components/Card.js
+++ src/components/Card.js
@@ -15,30 +15,29 @@ const Card = (props) => {
                     </div>
                 </Link>
                 :
-                <Link to={`/student-details/${props.id}`} key={props.i}>
-                    <div className="card student-card">
-                        <div className="card-body">
-                            <div className="student-profile">
-                                <div className="icon">
-                                    <img src={img} alt="{props.copy}" />
-                                </div>
-                                <h4 className="copy">{props.name}</h4>
+                <div className="card student-card" key={props.i}>
+                    <div className="card-body">
+                        <div className="student-profile">
+                            <div className="icon">
+                                <img src={img} alt="{props.copy}" />
                             </div>
-                            <div className="class">
-                                <label htmlFor="class">Class</label>
-                                <p>{props.class}</p>
-                            </div>
-                            <div className="roll">
-                                <label htmlFor="roll">Roll No</label>
-                                <p>{props.roll}</p>
-                            </div>
-                            <div className="year">
-                                <label htmlFor="year">Year</label>
-                                <p>{props.fromYear} - {props.toYear}</p>
-                            </div>
+                            <h4 className="copy">{props.name}</h4>
                         </div>
+                        <div className="class">
+                            <label htmlFor="class">Class</label>
+                            <p>{props.class}</p>
+                        </div>
+                        <div className="roll">
+                            <label htmlFor="roll">Roll No</label>
+                            <p>{props.roll}</p>
+                        </div>
+                        <div className="year">
+                            <label htmlFor="year">Year</label>
+                            <p>{props.fromYear} - {props.toYear}</p>
+                        </div>
+                        <button className="btn btn-sm btn-primary" onClick={() => studentDetails()}>show detail</button>
                     </div>
-                </Link>
+                </div>
             }
 
         </div>
